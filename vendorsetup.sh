ebd66c973a9cb39ed887855d68229bd76e82dce6
Itachi <Harshitverma54@gmail.com>
2020-08-15 07:08:20 +0000

    added vendorsetup.sh for caf repos
    Signed-off-by: Itachi <Harshitverma54@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+rm -rf packages/services/Telephony
+rm -rf frameworks/opt/telephony
+rm -rf frameworks/opt/net/ims
+rm -rf packages/services/Telecomm
+rm -rf packages/apps/OmniStyle
+git clone https://github.com/iitachiuchiha/android_packages_services_Telephony packages/services/Telephony
+git clone https://github.com/iitachiuchiha/android_frameworks_opt_telephony frameworks/opt/telephony
+git clone https://github.com/iitachiuchiha/android_frameworks_opt_net_ims frameworks/opt/net/ims
+git clone https://github.com/iitachiuchiha/android_packages_services_Telecomm packages/services/Telecomm
+git clone https://github.com/iitachiuchiha/Zenx_Omnistyle packages/apps/OmniStyle
